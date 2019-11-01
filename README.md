d455632ef5578dd734abef64c6d6059ab433713e
Yi Xue <20086419@mail.wit.ie>
2019-11-01 19:59:54 +0000

    getAllNovelsTest

---
+++ README.md
@@ -0,0 +1,54 @@
+# Novel-ideas
+
+
+Name: Yi Xue. Student number: 20086419
+
+In this project there are some functions:User can record and share the novels and authors the like in the app and manage them. 
+
+github:https://github.com/Ella-Xue/Novel-ideas.git
+// because the username Yi Xue is not available, Xue is the family name.
+user:
+  a post for register
+  
+  a post for login with username and password authentication
+  
+  a get for user to see his own information
+  
+  a put for user to change the password
+  
+novels:
+  a get to get all novels information
+  
+  a get to get a novel information
+  
+  a post to add new novels
+  
+  a put to give a grade to the novel
+  
+  a deleteto delete the novel
+  
+author:
+  a get to get all author information
+  
+  a get to get a author information
+  
+  a post to recommend a new author
+  
+  a put to collect the author, add 1 to the author’s number of collected
+  
+  a delete to remove the author
+  
+A web app developed with nodejs, express and mongodb. 
+NodeJS version v10.16.3 basic core development language
+NNPM version 6.9.0
+Express is a simple and flexible node.js Web application framework that offers a range of powerful features to help you create a variety of Web applications, as well as rich HTTP tools.
+Mongo version v4.2.1
+  
+  The attributes of each schema  persistence via mongodb.
+  Full source code in the github
+  
+  Reference:
+  
+  https://blog.csdn.net/weixin_30600197/article/details/97791975
+  
+  https://www.jianshu.com/p/74f98c0c8551
---
+++ test/api/novelTest.js
@@ -0,0 +1,106 @@
+const chai = require("chai");
+const expect = chai.expect;
+const request = require("supertest");
+const MongoMemoryServer = require("mongodb-memory-server").MongoMemoryServer;
+const Novel = require("../../models/novels");
+const mongoose = require("mongoose");
+
+const _ = require("lodash");
+let server;
+let mongod;
+let db, validID;
+
+describe("Novel", () => {
+    before(async () => {
+        try {
+            mongod = new MongoMemoryServer({
+                instance: {
+                    port: 27017,
+                    dbPath: "./test/database",
+                    dbName: "novels"
+                }
+            });
+            await mongod.getConnectionString();
+
+            mongoose.connect("mongodb://localhost:27017/novels", {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
+            server = require("../../bin/www");
+            db = mongoose.connection;
+        } catch (error) {
+            console.log(error);
+        }
+    })
+
+    after(async () => {
+        try {
+            await mongoose.connection.db.dropDatabase();
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    beforeEach(async () => {
+        try {
+            await Novel.deleteMany({});
+            let novel = new Novel();
+            novel.name = "Rusty Hotel";
+            novel.author = "Rusty";
+            novel.type = "Horror";
+            novel.recommender = "Merry";
+            await novel.save();
+            novel = new Novel();
+            novel.name = "Cube Escape";
+            novel.author = "Rusty";
+            novel.type = "Horror";
+            novel.recommender = "Merry";
+            await novel.save();
+            novel = await Novel.findOne({ name:"Rusty Hotel" });
+            validID = novel._id;
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    describe("GET/novels", () => {
+        it("should GET all the novels", done => {
+            request(server)
+                .get("/novels")
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .end((err, res) => {
+                    try {
+                        expect(res.body).to.be.a("array");
+                        expect(res.body.length).to.equal(2);
+                        let result = _.map(res.body, novel => {
+                            return {
+                                name: novel.name,
+                                author: novel.author,
+                                type: novel.type,
+                                recommender: novel.recommender,
+                            };
+                        });
+                        expect(result).to.deep.include({
+                            name:"Rusty Hotel",
+                            author:"Rusty",
+                            type:"Horror",
+                            recommender:"Merry"
+
+                        });
+                        expect(result).to.deep.include({
+                            name:"Cube Escape",
+                            author:"Rusty",
+                            type:"Horror",
+                            recommender:"Merry"
+
+                        });
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+        });
+    });
+});
